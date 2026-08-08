08cf775f095069ebf9229784fc5f71ff0125db92
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-07 17:31:56 -0700

    iniciando projeto

---
+++ meus-lidos-list/src/main/java/meus_lidos_list/MeusLidosListApplication.java
@@ -0,0 +1,13 @@
+package meus_lidos_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusLidosListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusLidosListApplication.class, args);
+	}
+
+}
---
+++ meus-lidos-list/src/test/java/meus_lidos_list/MeusLidosListApplicationTests.java
@@ -0,0 +1,13 @@
+package meus_lidos_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusLidosListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
