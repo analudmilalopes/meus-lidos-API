38c6e19c6dcd8cab2bdf84d9ac2606b538cfea49
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-08 12:24:37 -0700

    feat: criando mapper das Entitys

--- meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorMapper.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorMapper.java
@@ -1,4 +1,25 @@
 package meus_lidos_list.autor;
 
+
+import meus_lidos_list.autor.dto.AutorRequestDTO;
+import meus_lidos_list.autor.dto.AutorResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AutorMapper {
+
+    public AutorModel map(AutorRequestDTO autorDTO) {
+        AutorModel autorModel = new AutorModel();
+        autorModel.setNome(autorDTO.nome());
+
+        return autorModel;
+    }
+
+    public AutorResponseDTO map(AutorModel autorModel) {
+        return  new AutorResponseDTO(
+                autorModel.getNome(),
+                autorModel.getIdade(),
+                autorModel.getNacionalidade()
+                );
+    }
 }
--- meus-lidos-list/src/main/java/meus_lidos_list/livro/LivroMapper.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/livro/LivroMapper.java
@@ -1,4 +1,42 @@
 package meus_lidos_list.livro;
 
+import lombok.RequiredArgsConstructor;
+import meus_lidos_list.autor.AutorMapper;
+import meus_lidos_list.autor.dto.AutorResponseDTO;
+import meus_lidos_list.livro.dto.LivroRequestDTO;
+import meus_lidos_list.livro.dto.LivroResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class LivroMapper {
+
+    private final AutorMapper autorMapper;
+    public LivroModel map(LivroRequestDTO livroDTO) {
+        LivroModel livroModel = new LivroModel();
+        livroModel.setTitulo(livroDTO.titulo());
+        livroModel.setIsbn(livroDTO.isbn());
+        livroModel.setAnoPublicacao(livroDTO.anoPublicacao());
+        livroModel.setGenero(livroDTO.genero());
+        livroModel.setEditora(livroDTO.editora());
+        livroModel.setStatusLeitura(livroDTO.statusLeitura());
+        livroModel.setNota(livroDTO.nota());
+
+        return livroModel;
+    }
+
+
+    public LivroResponseDTO map(LivroModel livroModel) {
+            return new LivroResponseDTO(
+                    livroModel.getId(),
+                    livroModel.getTitulo(),
+                    livroModel.getIsbn(),
+                    livroModel.getAnoPublicacao(),
+                    livroModel.getGenero(),
+                    livroModel.getEditora(),
+                    livroModel.getStatusLeitura(),
+                    livroModel.getNota(),
+                    autorMapper.map(livroModel.getAutor())
+            );
+    }
 }
