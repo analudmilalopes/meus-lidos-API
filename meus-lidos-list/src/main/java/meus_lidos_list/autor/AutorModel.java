25a96808b52e863cc2831666165dc2835619e6f1
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-07 19:51:57 -0700

    feat: criando entitys Livro e Autor

--- meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorModel.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorModel.java
@@ -1,4 +1,39 @@
 package meus_lidos_list.autor;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import meus_lidos_list.livro.LivroModel;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+@Table(name = "autores")
 public class AutorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "idade", nullable = false)
+    private Integer idade;
+
+    @Column(name = "nacionalidade" , nullable = true, length = 15)
+    private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor_id")
+
+    @JsonIgnore
+    private List<LivroModel> livros;
+
+
 }
--- meus-lidos-list/src/main/java/meus_lidos_list/livro/LivroModel.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/livro/LivroModel.java
@@ -1,4 +1,49 @@
 package meus_lidos_list.livro;
 
+import enums.StatusLeitura;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import meus_lidos_list.autor.AutorModel;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
+@Entity
+@Table(name = "livros")
 public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false, length = 100)
+    private String titulo;
+
+    @Column(name = "ano_publicacao", nullable = false)
+    private Integer anoPublicacao;
+
+    @Column(name = "isbn", nullable = false,  length = 13)
+    private String isbn;
+
+    @Column(name = "genero", nullable = false, length = 100)
+    private String genero;
+
+    @Column(name = "editora", nullable = true)
+    private String editora;
+
+
+    @Column(name = "status_leitura", nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private StatusLeitura statusLeitura;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    AutorModel autor;
+
 }
