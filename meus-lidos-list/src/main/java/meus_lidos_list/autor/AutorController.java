edce02b8674c875fb8020c3805d34c2a61263ae5
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-08 20:38:38 -0700

    feat: criando controller Entity Autor

--- meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorController.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorController.java
@@ -1,4 +1,33 @@
 package meus_lidos_list.autor;
 
+import lombok.RequiredArgsConstructor;
+import meus_lidos_list.autor.dto.AutorRequestDTO;
+import meus_lidos_list.autor.dto.AutorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/autores")
+@RequiredArgsConstructor
 public class AutorController {
+
+    private final AutorRepository autorRepository;
+    private final AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<AutorResponseDTO> adicionarAutor(@RequestBody AutorRequestDTO autorDTO){
+        AutorResponseDTO autorNovo = autorService.cadastrar(autorDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(autorNovo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorResponseDTO>> listarAutores(){
+        List<AutorResponseDTO> autores = autorService.listarAutores();
+        return ResponseEntity.ok(autores);
+    }
 }
