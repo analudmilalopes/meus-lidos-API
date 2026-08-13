add9b0e777e54b2a9fdaacc6bbc8ad912ee2849a
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-13 15:59:27 -0700

    feat: cria regras de cada rota

--- meus-lidos-list/src/main/java/meus_lidos_list/livro/LivroService.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/livro/LivroService.java
@@ -3,9 +3,11 @@ package meus_lidos_list.livro;
 import lombok.RequiredArgsConstructor;
 import meus_lidos_list.autor.AutorModel;
 import meus_lidos_list.autor.AutorRepository;
+import meus_lidos_list.autor.dto.AutorRequestDTO;
 import meus_lidos_list.exception.ParametroNaoEncontrado;
 import meus_lidos_list.livro.dto.LivroRequestDTO;
 import meus_lidos_list.livro.dto.LivroResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -40,7 +42,38 @@ public class LivroService {
                 .collect(Collectors.toList());
     }
 
+    public LivroResponseDTO listarLivrosPorId (Long id) {
+
+        LivroModel livros = livroRepository.findById(id)
+                .orElseThrow(() -> new ParametroNaoEncontrado("Livro nao encontrado."));
+
+        return livroMapper.map(livros);
+
+    }
+
+    public List<LivroResponseDTO> listarLivrosPorAutorId(Long autorId) {
+
+        List<LivroModel> livros = livroRepository.findByAutorId(autorId);
+
+        return  livros.stream()
+                .map(livroMapper::map)
+                .collect(Collectors.toList());
+    }
 
 
+    public LivroResponseDTO alterarLivroPorId (Long id, LivroRequestDTO livroDTO) {
+            LivroModel livros = livroRepository.findById(id)
+                    .orElseThrow(() ->
+                            new ParametroNaoEncontrado("Livro nao encontrado."));
 
-}
+
+            LivroModel livroAtualizado = livroMapper.map(livroDTO);
+
+            livroAtualizado.setId(livros.getId());
+            livroAtualizado.setAutor(livros.getAutor());
+
+            LivroModel livroSalvo = livroRepository.save(livroAtualizado);
+
+            return livroMapper.map(livroSalvo);
+        }
+    }
