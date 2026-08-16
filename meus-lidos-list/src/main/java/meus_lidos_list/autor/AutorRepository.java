dacb79f9366b810d6d43ada617b7e517f8d2c35d
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-15 18:50:26 -0700

    feat: adicionando busca de autor pelo nome

--- meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorRepository.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorRepository.java
@@ -2,5 +2,10 @@ package meus_lidos_list.autor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AutorRepository extends JpaRepository<AutorModel, Long> {
+
+    Optional<AutorModel> findByNome(String nome);
 }
