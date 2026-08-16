337303e9eaf787ed24d74cd1eea7bdc6d59a4a03
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-15 18:53:11 -0700

    feat: adicionando anotacoes de validation

--- meus-lidos-list/src/main/java/meus_lidos_list/livro/dto/LivroRequestDTO.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/livro/dto/LivroRequestDTO.java
@@ -1,9 +1,14 @@
 package meus_lidos_list.livro.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import meus_lidos_list.enums.StatusLeitura;
 
 public record LivroRequestDTO(
         String titulo,
+
+        @NotNull(message = "Nao pode estar vazio!")
+        @Min(value = 10, message = "O valor minimo e 10")
         Integer anoPublicacao,
         String isbn,
         String genero,
