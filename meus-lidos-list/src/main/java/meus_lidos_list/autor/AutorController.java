ac95c8c2cc86708a7bb0633c6a61df704fa86251
Ludmila Lopes <ludmilalopes416@gmail.com>
2026-08-15 18:49:11 -0700

    feat: adicionando rotas de atualizar e buscar

--- meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorController.java
+++ meus-lidos-list/src/main/java/meus_lidos_list/autor/AutorController.java
@@ -13,8 +13,6 @@ import java.util.List;
 @RequestMapping("/api/autores")
 @RequiredArgsConstructor
 public class AutorController {
-
-    private final AutorRepository autorRepository;
     private final AutorService autorService;
 
     @PostMapping
@@ -27,7 +25,32 @@ public class AutorController {
 
     @GetMapping
     public ResponseEntity<List<AutorResponseDTO>> listarAutores(){
+
         List<AutorResponseDTO> autores = autorService.listarAutores();
+
         return ResponseEntity.ok(autores);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorResponseDTO> buscarAutorPorId(@PathVariable Long id){
+
+        AutorResponseDTO autor = autorService.listaAutorPorId(id);
+
+        return ResponseEntity.ok(autor);
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity <AutorResponseDTO> buscarAutorPorNome(@PathVariable String nome){
+
+       AutorResponseDTO autor = autorService.listarAutorPorNome(nome);
+
+        return ResponseEntity.ok(autor);
+    }
+
+    @PutMapping
+    public ResponseEntity <AutorResponseDTO> atualizarAutor(@PathVariable Long id, @RequestBody AutorRequestDTO autorDTO){
+        AutorResponseDTO autor = autorService.alterarAutorPorId(id, autorDTO);
+
+        return ResponseEntity.ok(autor);
     }
 }
